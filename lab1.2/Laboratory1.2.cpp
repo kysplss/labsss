65ca1b56de177e989aca9cf79322a5d20f8c4dd0
kysplss <its_unknown@icloud.com>
2025-04-09 23:27:17 +0300

    Create Laboratory1.2.cpp

---
+++ lab1.2/Laboratory1.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Player.h"
+#include "Enemy.h"
+#include "Boss.h"
+
+int main() {
+    // Создаем объекты игрока и врага
+    Player hero("Hero", 100, 0);
+    Enemy monster("Goblin", 50, "Goblin");
+    Boss boss("Haggard", 90, "Naga");
+
+    // Выводим информацию о персонажах
+    hero.displayInfo();
+    monster.displayInfo();
+    boss.displayInfo();
+
+    return 0;
+}
