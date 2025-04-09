9769986bdc5306a5564e8632d25a6d08be162a73
kysplss <its_unknown@icloud.com>
2025-04-09 23:35:56 +0300

    Create Laboratory4.cpp

---
+++ lab4/Laboratory4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "Inventory.h"
+#include <memory>
+#include <string>
+
+int main() {
+    //// Массив умных указателей на базовый класс
+    //std::unique_ptr<Entity> entities[] = {
+    //    std::make_unique<Player>("Hero", 100, 0),
+    //    std::make_unique<Enemy>("Goblin", 50, "Goblin")
+    //};
+
+    //// Полиморфизм и автоматическое управление памятью
+    //for (const auto& entity : entities) {
+    //    entity->displayInfo();
+    //}
+
+    std::unique_ptr<Inventory> inventory(new Inventory());
+
+    inventory->addItem("knife");
+    inventory->addItem("sword");
+    inventory->addItem("poison");
+    
+    inventory->displayInventory();
+
+    return 0;
+}
