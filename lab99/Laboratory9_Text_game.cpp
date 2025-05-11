fc47ad47c88f7a01ebbdabd41819ad8347839c60
kysplss <its_unknown@icloud.com>
2025-05-12 00:37:49 +0300

    Create Laboratory9_Text_game.cpp

---
+++ lab99/Laboratory9_Text_game.cpp
@@ -0,0 +1,32 @@
+﻿#include "Game.h"
+#include "Logger.h"
+#include <iostream>
+
+int main() {
+    try {
+        Logger<std::string> logger("game_log.txt");
+        Game game("Hero");
+
+        logger.log("Game started.");
+        game.startGame();
+
+        logger.log("Starting first fight.");
+        game.fight();
+
+        logger.log("Saving progress.");
+        game.saveProgress("savegame.txt");
+
+        logger.log("Loading progress.");
+        game.loadProgress("savegame.txt");
+
+        logger.log("Starting second fight.");
+        game.fight();
+
+    }
+    catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
